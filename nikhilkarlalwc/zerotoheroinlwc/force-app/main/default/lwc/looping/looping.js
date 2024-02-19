421f5a8983e61abd7f2c697bd13e7c296570b408
unknown <kumarkumar.talk@gmail.com>
2024-02-19 19:12:12 +0530

    updated commit

---
+++ nikhilkarlalwc/zerotoheroinlwc/force-app/main/default/lwc/looping/looping.js
@@ -0,0 +1,29 @@
+import { LightningElement } from 'lwc';
+
+export default class Looping extends LightningElement {
+
+    carlist=['BMW','AUDI','LEMBO','HUNDAI','KIA'];
+
+    ceolist=[
+        {
+            id:1,
+            company:'Google',
+            name:'Sundar Pchai'
+        },
+        {
+            id:2,
+            company:'Mark Zukerberg',
+            name:'META'
+        },
+        {
+            id:3,
+            company:'Space X',
+            name:'Elon Musk'
+        },
+        {
+            id:4,
+            company:'KUMARTECH 7',
+            name:'VIKAS KUMAR'
+        },
+    ];
+}
